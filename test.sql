b10c52f11a2def7e83bc8b91b66dbebafba3c18f
suraksha29 <94095269+suraksha29@users.noreply.github.com>
2021-12-26 07:16:33 -0800

    Create test.sql

---
+++ test.sql
@@ -0,0 +1,9 @@
+create database test;
+
+use test;
+
+create table register( id int primary key auto_increment,name varchar(255),password varchar(255),emai varchar(255),sex varchar(100),country varchar(250));
+ 
+ 
+ select * from  register;
+ 
